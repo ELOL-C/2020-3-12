6955951a2a8c32d2aaad6cbf0577aa94b13144be
ZEZE <55626553+EZ-Super@users.noreply.github.com>
2020-03-12 21:33:31 +0800

    Add files via upload

---
+++ 站名.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>   
+using namespace std;
+
+int main()
+{
+	double a,c,b  ;
+	string str ;
+	rep_in:	
+	cout << "請輸入站名-";
+	cin >> str ;
+	if (str == "二林" ){
+		cout << str<<"站需要30元";
+	}
+	else if (str == "溪湖"){
+		cout << str<<"站需要45元";
+	}
+	else if (str == "彰化"){
+		cout << str<<"站需要50元";
+	}
+	else if (str == "員林"){
+		cout << str<<"站需要60元";
+	}
+	else {
+		cout << "找不到" << str <<"這個站名\n" ; 
+		system("Pause");
+		goto rep_in;
+	}
+	
+return 0;	
+}
